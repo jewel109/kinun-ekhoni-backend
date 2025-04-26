51dd4e7b8affe1deb5308fab220138a2fcc45a87
jewel109 <raihanurrahmanjewel@gmail.com>
2025-04-26 23:06:57 +0600

    merge conflict solved

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,42 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt-auth.gurd';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { SuccessInterceptor } from './success.interceptor';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<TypeOrmModuleOptions> => ({
+        type: 'mysql',
+        host: 'localhost',
+        port: configService.get('DB_PORT'),
+        username: configService.get('MYSQL_USER'),
+        password: configService.get('MYSQL_PASSWORD'),
+        database: configService.get('MYSQL_DATABASE'),
+        entities: [User],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard
-    }
+    },
+    { provide: APP_INTERCEPTOR, useClass: SuccessInterceptor }
   ],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CatchEverythingFilter } from './error.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new CatchEverythingFilter(httpAdapter));
+  await app.listen(port);
+  console.log(`server is listenning on port ${port}`)
 }
 bootstrap();
