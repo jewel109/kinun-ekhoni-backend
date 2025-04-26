67c94e1be0bcc426e965bfcfa3e2e793f7b144ac
jewel109 <raihanurrahmanjewel@gmail.com>
2025-04-26 22:59:41 +0600

    succes interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.gurd';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
