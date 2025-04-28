9dbbda20b050de665c0c10957d4f95e92b2b15c7
jewel109 <raihanurrahmanjewel@gmail.com>
2025-04-28 20:52:48 +0600

    user entity updated

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,5 +1,9 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
+export enum UserRole {
+  ADMIN = 'admin',
+  SELLER = 'seller',
+  BUYER = 'buyer',
+}
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
@@ -13,4 +17,11 @@ export class User {
 
   @Column()
   password: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.SELLER
+  })
+  role: UserRole;
 }
